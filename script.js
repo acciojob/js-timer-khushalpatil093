e1892f7c27b76a094405efb85e7803ddafdb3082
Khushal Patil <58685911+khushalpatil093@users.noreply.github.com>
2023-09-06 18:42:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,10 +3,10 @@
 	const date = new Date();
 
 	const day = date.getDate().toString().padStart(2, '0');
-	const month = (date.getMonth()+1).toStrinng().padStart(2, '0');
+	const month = (date.getMonth()+1).toString().padStart(2, '0');
 	const year = date.getFullYear();
 	const hours = date.getHours().toString().padStart(2, '0');
 	const minutes = date.getMinutes().toString().padStart(2, '0');
-	const seconds = date.getSeconds().toString().padStrt(2, '0');
+	const seconds = date.getSeconds().toString().padStart(2, '0');
 
 	Time.innerHTML = `${day}/${month}/${year}, ${hours}:${minutes}:${seconds}`;
