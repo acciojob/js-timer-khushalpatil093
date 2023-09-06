2849b66d2f13289d2cc6a1442f981605d2163a4a
Khushal Patil <58685911+khushalpatil093@users.noreply.github.com>
2023-09-06 18:40:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // write js code here if required
-	let dateTime = document.getElementById('timer');
+	let Time = document.getElementById('timer');
 	const date = new Date();
 
-	const day = date.getDay().toString().padStart(2, '0');
+	const day = date.getDate().toString().padStart(2, '0');
 	const month = (date.getMonth()+1).toStrinng().padStart(2, '0');
 	const year = date.getFullYear();
 	const hours = date.getHours().toString().padStart(2, '0');
 	const minutes = date.getMinutes().toString().padStart(2, '0');
 	const seconds = date.getSeconds().toString().padStrt(2, '0');
 
-	dateTime.innerHTML = `${day}/${month}/${year}, ${hours}:${minutes}:${seconds}`;
+	Time.innerHTML = `${day}/${month}/${year}, ${hours}:${minutes}:${seconds}`;
